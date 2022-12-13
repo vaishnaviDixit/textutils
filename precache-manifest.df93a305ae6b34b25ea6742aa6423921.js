c0cdcae824e6db374dd0d5d78c3a00430497a275
vaishnaviDixit <vaishu765201@gmail.com>
2022-12-13 22:31:12 +0530

    Updates

---
+++ precache-manifest.df93a305ae6b34b25ea6742aa6423921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19aea52225f87b1897f5",
+    "url": "/textutils/static/css/main.35c19f1f.chunk.css"
+  },
+  {
+    "revision": "19aea52225f87b1897f5",
+    "url": "/textutils/static/js/main.19aea522.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/textutils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/textutils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "168bd853d713a82610a7",
+    "url": "/textutils/static/js/runtime~main.168bd853.js"
+  },
+  {
+    "revision": "d91e76e4b335babcd436fd0619c2038e",
+    "url": "/textutils/index.html"
+  }
+];
